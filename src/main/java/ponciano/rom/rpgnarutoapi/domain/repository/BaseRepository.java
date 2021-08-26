f73a913bfcc6448718439bb9f51396a6a85a4a51
Rômulo Ponciano <romponciano@gmail.com>
2021-08-26 00:30:00 -0300

    change package name

--- src/main/java/ponciano/rom/rpgnarutoapi/domain/repository/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package ponciano.rom.rpgnarutoapi.domain.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public abstract class BaseRepository<E> implements MongoRepository<E, String> {
-}
